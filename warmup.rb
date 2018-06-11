7491409e57c0d71ed917da403ac9fd06e271c0d2
Natalia0727 <oicafn@gmail.com>
2018-06-11 16:05:50 +0000

    automatically backed up by learn

--- warmup.rb
+++ warmup.rb
@@ -1,1 +1,21 @@
 class Dog
+  def name=(dog_name)
+    @this_dog_name = dog_name
+  end 
+  def name 
+    @this_dog_name
+  end 
+  
+  def breed=(dog_breed)
+    @this_dog_breed = dog_breed
+  end 
+  def breed 
+    @this_dog_breed
+  end 
+  def bark
+    puts "woof!"
+  end 
+end 
+kenji=Dog.new 
+kenji.name ="kenji"
+kenji.breed = "cocker spaniel"
